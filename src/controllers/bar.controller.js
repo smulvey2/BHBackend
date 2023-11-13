120e86515bf3fcc1e4922b82e433fe1ed8bd9d84
Steven Mulvey <33791425+smulvey2@users.noreply.github.com>
2023-11-13 14:50:59 -0600

    Update bar.controller.js

--- src/controllers/bar.controller.js
+++ src/controllers/bar.controller.js
@@ -1,7 +1,7 @@
 const {Sequelize, Op} = require('sequelize')
 const BarService = require("../services/bars/bars.service.js");
 const db = require("../models/db.js");
-const service = new BarService(db.models.importTest); 
+const service = new BarService(db.models.bars); 
 
 exports.getBarsByLocation = async (req, res, next) => {
     console.log('Nearby Bars')
